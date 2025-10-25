fc68d28cc300b47a1361431dfcae8b310d782f6f
Yosri Fathallah <fthyosri@gmail.com>
2025-10-25 18:36:57 +0100

    baby step

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View style={{ backgroundColor: "red", flex: 1 }}>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
